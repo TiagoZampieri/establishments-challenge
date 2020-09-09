26d793115df4eae995dce89347b330138ff53139
zampieri <tzampieri7@gmail.com>
2020-09-09 10:01:18 -0300

    Ajustes na rota e readme.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'establishment', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }
+  { path: 'establishment',
+    loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'establishment'
+  }
+
 ];
 
 @NgModule({
